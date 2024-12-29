b8d8d15105fdfa71dc75ed5bde64b41893c1d83e
Rafal Duszynski <rduszyn1@stu.vistula.edu.pl>
2024-12-29 16:39:20 +0100

    Initial commit

---
+++ src/main/java/pl/edu/vistula/first_project_java_spring_part_1/FirstProjectJavaSpringPart1Application.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_java_spring_part_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectJavaSpringPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectJavaSpringPart1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/vistula/first_project_java_spring_part_1/FirstProjectJavaSpringPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_java_spring_part_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectJavaSpringPart1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
